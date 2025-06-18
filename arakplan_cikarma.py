9c4afc2b3aefbf61fa52188655db72954cb7f8f6
salim soy <salimsoy0320@gmail.com>
2025-06-18 12:06:47 +0300

    pep8 standatlarına uygun hale getirildi

--- arakplan_cikarma.py
+++ arakplan_cikarma.py
@@ -2,53 +2,67 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('input.jpg')
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-edges = cv2.Canny(gray, threshold1=100, threshold2=200)
-
-kernel_close = np.ones((9, 9), np.uint8)
-iterasyon = 3
-
-closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel_close, iterasyon)
-
-contours, _ = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-temp = []
-for i in range(len(contours)):
-    if(i == 0 or i == 3 or i == 4 or i == 9):
-        continue
-    temp.append(contours[i])
+class BackgroundSubtraction:
     
-contours_sift = tuple(temp)
+    def __init__(self, img):
+        self.img = img
 
-lower_red = np.array([19, 39, 0])
-upper_red = np.array([180, 255, 255])
-
-mask_range = cv2.inRange(hsv, lower_red, upper_red)
-contours_range, _ = cv2.findContours(mask_range, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-contour_range_mask = np.zeros_like(gray)
-cv2.drawContours(contour_range_mask, [contours_range[0]], -1, 255, thickness=cv2.FILLED)
+    def convert_to_gray_and_hsv(self):
+        self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        self.hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        return self.gray, self.hsv
     
-contour_mask = np.zeros_like(gray)
-cv2.drawContours(contour_mask, contours_sift, -1, 255, thickness=cv2.FILLED)
+    def edge_detection(self):
+        self.edges = cv2.Canny(self.gray, threshold1=100, threshold2=200)
+        
+    def morphological_closing(self):
+        kernel_close = np.ones((9, 9), np.uint8)
+        iterasyon = 3
+        self.closing = cv2.morphologyEx(self.edges, cv2.MORPH_CLOSE, kernel_close, iterasyon)
 
-result = cv2.bitwise_or(contour_mask, contour_range_mask)
+    def find_contours(self):
+        contours, _ = cv2.findContours(self.closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        temp = []
+        for i in range(len(contours)):
+            if(i == 0 or i == 3 or i == 4 or i == 9):
+                continue
+            temp.append(contours[i])
+    
+        self.contours_sift = tuple(temp)
+        
+    def in_range(self):
 
+        lower_red = np.array([19, 39, 0])
+        upper_red = np.array([180, 255, 255])
+        mask_range = cv2.inRange(self.hsv, lower_red, upper_red)
+        self.contours_range, _ = cv2.findContours(mask_range, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    def draw_contours(self):
+        self.contour_range_mask = np.zeros_like(self.gray)
+        cv2.drawContours(self.contour_range_mask, [self.contours_range[0]], -1, 255, thickness=cv2.FILLED)
+    
+        self.contour_mask = np.zeros_like(self.gray)
+        cv2.drawContours(self.contour_mask, self.contours_sift, -1, 255, thickness=cv2.FILLED)
 
+    def bitwise_or(self):
+        self.result = cv2.bitwise_or(self.contour_mask, self.contour_range_mask)
+        return self.result
 
+if __name__ == '__main__':
+    img = cv2.imread('input.jpg')
+    process = BackgroundSubtraction(img)
+    process.convert_to_gray_and_hsv()
+    process.edge_detection()
+    process.morphological_closing()
+    process.find_contours()
+    process.in_range()
+    process.draw_contours()
+    result = process.bitwise_or()
+    
+    cv2.imwrite("output.jpg", result)
+    
+    cv2.imshow("Sonuc", result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imwrite("output.jpg", result)
-
-cv2.imshow("kontur range", contour_range_mask)
-cv2.imshow("kontur", contour_mask)
-cv2.imshow("Sonuc", result)
-cv2.imshow("Orijinal", img)
-cv2.imshow("gri", gray)
-cv2.imshow("canny", edges)
-cv2.imshow("close", closing)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
